f716ad45c0abc7a23477b811feb61712f0f9c8bf
NofiQ <ptinexx@proton.me>
2025-08-22 14:59:48 +0000

    Cleanup root, move code fully into src/

--- config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-SMTP_USER = "bodyaraz7@gmail.com"
-SMTP_PASS = "hqmmpdzlupuyvijl"
-DATABASE_URL = "sqlite:///./bulletin_board.db"

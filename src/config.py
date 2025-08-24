44aacb8896006b229ad940848e82f856f2a47c45
bogdan123824 <156796479+bogdan123824@users.noreply.github.com>
2025-08-24 23:12:39 +0300

    #Fix config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
+import os
+
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 SMTP_USER = "bodyaraz7@gmail.com"
 SMTP_PASS = "hqmmpdzlupuyvijl"
-DATABASE_URL = "sqlite:///./bulletin_board.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, '..', 'bulletin_board.db')}"
 BASE_URL = "https://api.beez.pp.ua"
 
